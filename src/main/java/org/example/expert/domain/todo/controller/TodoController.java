c6b7310d99841a89912bebf9f31f56e14e90f425
Jiwon Yang <g.one9709@gmail.com>
2026-06-17 17:21:29 +0900

    feat: 날씨와 수정일 기준 할 일 검색 추가

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -9,8 +9,11 @@ import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,12 +29,26 @@ public class TodoController {
         return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
     }
 
+    /**
+     *
+     * @param page
+     * @param size
+     * @param weather
+     * @param modifiedFrom 수정일 시작일
+     * @param modifiedTo 수정일 종료일
+     * @return
+     */
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(name ="weather",required = false) String weather,
+            @RequestParam(name = "modifiedFrom", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime modifiedFrom,
+            @RequestParam(name = "modifiedTo", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime modifiedTo
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, modifiedFrom, modifiedTo));
     }
 
     @GetMapping("/todos/{todoId}")
